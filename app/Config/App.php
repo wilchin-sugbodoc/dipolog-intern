07bce8e0a6682d6a2ecd0e20ae98c4c03cf3ca65
Wilchin Dipolog <wilchin.sugbodoc@gmail.com>
2025-02-20 14:20:13 +0800

    modified chat.php added necessary js and plugins for notifications (including the chat.js, notifIt.js, and jquery.growl.js) and its CSS (including notifIt.css and jquery.growl.css). Added onclick function to the send button and uses not7() method. For "Call is ongoing" notification message, I change the value of href into "#" to not refresh the page and added class "call" to the call icon.     modified chat.js copied from sample.js and pasted to the chat.js to add new function which is not7() using the type "success" and display the message "Message has been sent". I also copied, pasted, and changed its values of the function to display the message "Call is ongoing" to the chat.js

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://192.168.68.145:8080/rygel-dash-theme/';
+	public $baseURL = 'http://localhost:8080/rygel-dash-theme/';
 
 	/*
 	|--------------------------------------------------------------------------
